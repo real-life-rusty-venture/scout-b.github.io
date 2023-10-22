263f4efdc8f83d223a32e5035dd01139b1c28e43
real-life-rusty-venture <ariannabrunson47@gmail.com>
2023-10-22 12:26:37 -0700

    my portfolio
    first portfolio website

---
+++ client/src/main.jsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+); 
